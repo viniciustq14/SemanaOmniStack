6fec547cb43381f21120180bc2b0aee4ad94eb46
unknown <viniciustq14@bol.com.br>
2020-03-26 23:32:13 -0300

    arquivos do DB e de rotas

---
+++ SemanaOmniStack11/backend/src/database/migrations/20200326232232_create.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    knex.schema.createTable('incidents',table=>{
+        table.increments();
+        table.string('title').notNullable();
+        table.string('descriptio').notNullable();
+        table.decimal('value').notNullable();
+        table.string('ong_id').notNullable();
+        table.foreign('ong_id').references('id').inTable('ongs');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+};
