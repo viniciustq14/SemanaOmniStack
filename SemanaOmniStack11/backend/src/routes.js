66ecc67342089d2e2659e93f807ce74e190509c0
unknown <viniciustq14@bol.com.br>
2020-03-27 23:06:23 -0300

    rota funcionando

--- SemanaOmniStack11/backend/src/routes.js
+++ SemanaOmniStack11/backend/src/routes.js
@@ -1,8 +1,19 @@
 const express=require('express');
+const ongController=require('./controllers/ongController')
+const incidentController=require('./controllers/incidentController');
+const profileCotroller=require('./controllers/profileController');
+const sectionCotroller=require('./controllers/sectionController');
 const routes =express.Router();
 
-routes.get('/',(req,res)=>{
-    
-});
+routes.get('/ongs',ongController.index);
+routes.post('/ongs',ongController.create);
+
+routes.post('/sections',sectionCotroller.create)
+
+routes.get('/porfile',profileCotroller.index);
+
+routes.get('/incidents',incidentsController.index);
+routes.post('/incidents',incidentsController.create);
+routes.delete('/incidents/:id',incidentsController.delete);
 
 module.exports=routes;
