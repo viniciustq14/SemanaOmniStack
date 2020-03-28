c7f3fafb3c482a1494bd38c378551986dec7c4aa
unknown <viniciustq14@bol.com.br>
2020-03-28 09:51:11 -0300

    identado

--- SemanaOmniStack11/backend/src/controllers/incidentController.js
+++ SemanaOmniStack11/backend/src/controllers/incidentController.js
@@ -1,39 +1,43 @@
-const crypto=require('crypto');
-const connection=require('../database/connection');
+const crypto = require('crypto');
+const connection = require('../database/connection');
 
-module.exports={
-   async create(req,res){
-    const {title,description,value}=req.body;
-    const ong_id=req.headers.authorization;
+module.exports = {
+    async create(req, res) {
+        const { title, description, value } = req.body;
+        const ong_id = req.headers.authorization;
 
-   const [id]= await connection('incidents').insert({
-        title,
-        description,
-        value,
-        ong_id
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
 
-    })
+        })
 
-    return res.json({id});
-},
-async index (req,res){
-    const { page - 1 }=req.query;
-    const [count]=await connection('incidents').count();
-    const incidents =await connection('incidents').join('ongs','ongs.id','=','incidents.ong_id').limit(5).offset({page-1}*5)select(['incidents.*','ongs.name','ongs.email','ongs.whatsapp','ongs.city','ongs.uf']);
-    res.header('X-Total-Count',count['count(*)'])
-    return res.json(incidents);
-},
-async delete(req,res){
-    const ong_id=req.headers.authorization;
-    const {id}=req.params;
+        return res.json({ id });
+    },
+    async index(req, res) {
+        const { page } = req.query;
+        page--;
+        const [count] = await connection('incidents').count();
+        const incidents = await connection('incidents').
+            join('ongs', 'ongs.id', '=', 'incidents.ong_id').
+            limit(5).offset(page * 5).
+            select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+        res.header('X-Total-Count', count['count(*)'])
+        return res.json(incidents);
+    },
+    async delete(req, res) {
+        const ong_id = req.headers.authorization;
+        const { id } = req.params;
 
-    const incidents= await connection('incidents').where('id',id).select('ong_id').fist();
+        const incidents = await connection('incidents').where('id', id).select('ong_id').fist();
 
-    if(incidents.ong_id!=ong_id){
-        return res.status(401).json({error:'Operation not permited'});
+        if (incidents.ong_id != ong_id) {
+            return res.status(401).json({ error: 'Operation not permited' });
+        }
+        await connection('incidents').where('id', id).delete();
+        return res.status(201).send();
+
     }
-await connection('incidents').where('id',id).delete();
-return res.status(201).send();
-
-}
 }
